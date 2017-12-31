670795b455c28304c43bd84671ae68a3b1cd1e7c
xeon365 <xee3oon@gmail.com>
2017-12-31 13:00:39 +0000

    Test

--- Commands/commands.js
+++ Commands/commands.js
@@ -28,6 +28,33 @@ module.exports = function(bot)
             
         }, 1000);
         
-        
+        bot.registerCommand("test", (msg, args) => {
+            bot.createMessage(msg.channel.id, {
+                embed: {
+                    title: "I'm an embed!", // Title of the embed
+                    description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
+                    author: { // Author property
+                        name: msg.author.username,
+                        icon_url: msg.author.avatarURL
+                    },
+                    color: 0x008000, // Color, either in hex (show), or a base-10 integer
+                    fields: [ // Array of field objects
+                        {
+                            name: "Some extra info.", // Field title
+                            value: "Some extra value.", // Field
+                            inline: true // Whether you want multiple fields in same line
+                        },
+                        {
+                            name: "Some more extra info.",
+                            value: "Another extra value.",
+                            inline: true
+                        }
+                    ],
+                    footer: { // Footer text
+                        text: "Created with Eris."
+                    }
+                }
+            });
+        });
     })
 }
