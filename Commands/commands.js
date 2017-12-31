829b21a5c5552f4d14febf1edfff9d1bd0afc8c8
xeon365 <xee3oon@gmail.com>
2017-12-31 14:33:51 +0000

    Update

--- Commands/commands.js
+++ Commands/commands.js
@@ -1,14 +1,7 @@
 const request = require('request');
 module.exports = function(bot) 
 {
-    var misc = bot.registerCommand("misc", (msg, args) => {
-        if(args.length ===0)
-        {
-            return "Invalid Input";
-        }
-    })
-
-    misc.registerSubcommand("weather", (msg, args) => {
+    bot.registerCommand("weather", (msg, args) => {
         console.log("Received Message " + msg +" " +args);
         if(args.length === 0)
         {
@@ -23,38 +16,75 @@ module.exports = function(bot)
         });
         
         setTimeout(() => {
-            console.log(weather.weather[0].description);
-            bot.createMessage(msg.channel.id, "Weather: "+weather.weather[0].description);
             
-        }, 1000);
-
-        bot.createMessage(msg.channel.id, {
-            embed: {
-                title: "Weather for "+weather.name+", "+weather.sys.country, // Title of the embed
-                description: "",
-                author: { // Author property
-                    name: "Weather",
-                    icon_url: ":white_sun_cloud:"
-                },
-                color: 0x008000, // Color, either in hex (show), or a base-10 integer
-                fields: [ // Array of field objects
-                    {
-                        name: "Temp", // Field title
-                        value: weather.main.temp-273.15+"°C", // Field
-                        inline: true // Whether you want multiple fields in same line
+            //bot.createMessage(msg.channel.id, "Weather: "+weather.weather[0].description);
+            
+            bot.createMessage(msg.channel.id, {
+                embed: {
+                    title: "Weather for "+weather.name+", "+weather.sys.country, // Title of the embed
+                    description: ""+weather.weather[0].description,
+                    author: { // Author property
+                        name: "Weather",
+                        icon_url: "https://openweathermap.org/img/w/"+weather.weather[0].icon+".png"
                     },
-                    {
-                        name: "Wind",
-                        value: weather.wind.speed+"m/S",
-                        inline: true
-                    }
-                ]
-            }
-        });
+                    color: 0x008000, // Color, either in hex (show), or a base-10 integer
+                    fields: [ // Array of field objects
+                        {
+                            name: "Temp", // Field title
+                            value: round(weather.main.temp-273.15,2)+"°C", // Field
+                            inline: true // Whether you want multiple fields in same line
+                        },
+                        {
+                            name: "Humidity",
+                            value: weather.main.humidity+"%",
+                            inline: true
+                        },
+                        {
+                            name: "Wind",
+                            value: weather.wind.speed+" m/s",
+                            inline: true
+                        },
+                        {
+                            name: "Wind2",
+                            value: weather.wind.speed+"m/S",
+                            inline: false
+                        }
+                    ]
+                    
+                }
+            });
+        }, 1000);
     })
 
-    bot.registerCommand("test", (msg, args) => {
-        
-        
+    function round(value, precision) {
+        var multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+    }
+
+    bot.registerCommand("8ball", (msg, args) => {
+        var data;
+        request('https://8ball.delegator.com/magic/JSON/'+args, {json: true}, (err,res,body) => {
+            if(err) { return err};
+            
+            data = body;
+        });
+        setTimeout(() => 
+        {
+            bot.createMessage(msg.channel.id, "[:8ball:] **8Ball**: **"+data.magic.answer+"**");
+        },1000);
+    });
+
+    bot.registerCommand("yoda", (msg, args) => {
+        var data;
+        console.log("Message Received: "+msg+" Args: "+args)
+        request('http://api.funtranslations.com/translate/yoda.json?text='+args.join(" "), {json: true}, (err,res,body) => {
+            if(err) { return err};
+            
+            data = body;
+        });
+        setTimeout(() => 
+        {
+            bot.createMessage(msg.channel.id, "[:yoda:] **Yoda**: **"+data.contents.translated+"**");
+        },1000);
     });
 }
